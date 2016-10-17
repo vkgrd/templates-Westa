53030afaaa577d89054ac0495df3fc9fd49abbf8
Denis Hardenberg <aaronbe@ya.ru>
2016-10-17 16:48:25 +0500

    Header & Top Section

---
+++ src/js/dev.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(){
+	var scrollTop = $(window).scrollTop();
+	if(scrollTop > 145) {
+		$('.header').addClass('scrolled');
+	} else {
+		$('.header').removeClass('scrolled');
+	}
+});
